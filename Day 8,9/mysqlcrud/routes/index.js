5f2d7c7cc21876eb3316841fd6d9685f49e40447
sachi1911 <66085594+sachi1911@users.noreply.github.com>
2021-06-10 15:06:23 +0530

    crud operation

---
+++ Day 8,9/mysqlcrud/routes/index.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var mysql=require('mysql');
+var router = express.Router();
+var connection=mysql.createConnection({
+  host : 'localhost',
+  user :'root',
+  password:'',
+  database:'nodejsdemo'
+});
+
+connection.connect(function(err){
+  if(!err){
+    console.log('Db connected'); 
+  }
+  else{
+    console.log('Db connection issue');
+  }
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/add', function(req, res, next) {
+  res.render('add',{ title: 'Express' });
+});
+
+router.post('/add-process', function(req, res, next) {
+  console.log(req.body);
+
+  const mybodydata={
+    product_name:req.body.txt1,
+    product_detail:req.body.txt2,
+    product_price:req.body.txt3,
+  }
+
+  connection.query("insert into tbl_product set?",mybodydata,function(err,result){
+    if(err) throw err;
+    res.redirect('/add')
+  });
+});
+router.get('/display', function(req, res, next) {
+  connection.query("select * from tbl_product",function(err,db_rows){
+    if(err) throw err;
+    console.log(db_rows);
+    res.render('display',{db_rows_array:db_rows});
+  });
+});
+router.get('/delete/:id', function(req, res, next) {
+  var deleteid = req.params.id;
+  console.log(deleteid);
+  connection.query("delete from tbl_product where product_id =?",[deleteid],function(err,db_rows){
+    if(err) throw err;
+    console.log(db_rows);
+    res.redirect('/display');
+  });
+});
+
+router.get('/edit/:id', function(req, res, next) {
+  var editid = req.params.id;
+  console.log(editid);
+  connection.query("select * from tbl_product where product_id =?",[editid],function(err,db_rows){
+    if(err) throw err;
+    console.log(db_rows);
+    res.render('edit',{db_rows_array:db_rows});
+  });
+});
+
+router.post('/edit/:id', function(req, res, next) {
+  var editid = req.params.id;
+  var pname = req.body.txt1;
+  var pdetail = req.body.txt2;
+  var pprice = req.body.txt3;
+
+  connection.query("update tbl_product set product_name =?,product_detail=?,product_price=? where product_id=?",[pname,pdetail,pprice,editid],function(err,db_rows){
+    if(err) throw err;
+    res.redirect('/display');
+  });
+});
+
+module.exports = router;
