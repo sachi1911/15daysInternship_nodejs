25b68a3d670c4a2296c4a996c223650026875d4f
sachi1911 <66085594+sachi1911@users.noreply.github.com>
2021-06-10 23:37:47 +0530

    ejs

---
+++ Day 5,6,7/task/routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/home', function(req, res, next) {
+  res.render('master', { title: 'Express' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('About', { title: 'Express' });
+});
+router.get('/form', function(req, res, next) {
+  res.render('form');
+});
+router.post('/form-process', function(req, res, next) {
+  res.render('ans');
+});
+module.exports = router;
